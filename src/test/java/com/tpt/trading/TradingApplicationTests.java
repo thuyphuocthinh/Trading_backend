413f1b9d849d6703dd9fa68bbc3b8ac91bf67476
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2025-04-30 16:04:59 +0700

    [main] init project

---
+++ src/test/java/com/tpt/trading/TradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpt.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
